4b00787060964719b71faed778d1f2eb1eecbc8c
Nobuhito Kurose <Nobu@kurosenobuhito-no-MacBook-Air.local>
2016-07-10 10:48:14 +0900

    add rinna

--- index.js
+++ index.js
@@ -17,3 +17,44 @@ line.listen(3000).then((res) => {
     states[content.from] = newState;
   }
 });
+
+
+//rinna
+var Twit = require('twit');
+const RINNA_TWITTER_ID = '3274075003';
+var T = new Twit({
+  consumer_key: process.env.CONSUMER_KEY,
+  consumer_secret: process.env.CONSUMER_SECRET,
+  access_token: process.env.ACCESS_TOKEN,
+  access_token_secret: process.env.ACCESS_TOKEN_SECRET
+});
+
+function sendDirectMessageToRinna(message){
+  T.post('direct_messages/new', { user_id: RINNA_TWITTER_ID, text: message }, function (err, data, response) {
+    if (err){
+      console.log(err);
+    }else{
+      console.log('message sent');
+    }
+  });
+}
+
+var stream = T.stream('user');
+
+stream.on('connect', function () {
+   console.log('\nuser stream connecting..')
+})
+stream.on('connected', function () {
+   console.log('user stream connected.')
+})
+
+//ユーザーの各種イベントを監視
+// stream.on('user_event', function (data) {
+//    console.log(data.event);
+// })
+
+//ユーザーのDMを監視
+stream.on('direct_message', function (data){
+  console.log(data.direct_message.sender_id);
+  console.log(data.direct_message.text);
+});
