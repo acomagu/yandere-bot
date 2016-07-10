258595db70fa17104f299d12f1ebcea2dd93046a
Yuki Ito <acomagu@gmail.com>
2016-07-10 11:49:01 +0900

    add port env

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const line = require('./line.js');
 
 let states = {};
 
-line.listen(3000).then((res) => {
+line.listen(process.env.PORT || 3000).then((res) => {
   const content = res.content;
   if(content.contentType === LineBot.CONST.CONTENT_TYPE.TEXT) {
     const user = new line.User(bot, content.from);
